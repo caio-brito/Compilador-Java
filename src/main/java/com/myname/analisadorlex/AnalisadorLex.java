bb0eec623e27ea41078746af0e3c6c6bbf1f0d0b
Caio Brito <caio@300E5K-300E5Q>
2022-09-09 21:56:04 -0300

    penultimas alteracoes

--- src/main/java/com/myname/analisadorlex/AnalisadorLex.java
+++ src/main/java/com/myname/analisadorlex/AnalisadorLex.java
@@ -3,7 +3,6 @@
 *Caio Vinicius Oliveira Brito RA:2150905
 *Vitor Augusto Ozanick        RA:2152401 
  */
-
 package com.myname.analisadorlex;
 
 import java.io.IOException;
@@ -12,7 +11,7 @@ import java.util.StringTokenizer;
 public class AnalisadorLex {
 
     public static void main(String[] args) throws IOException {
-        
+
         Leitura l = new Leitura(args[0]);
         GyhLex identChar = new GyhLex();
         String LinhaLida, teste, teste1, junta;
@@ -24,7 +23,7 @@ public class AnalisadorLex {
 
         while (LinhaLida != null) {
 
-            StringTokenizer st = new StringTokenizer(LinhaLida, " :#()<=>+-==@^&|~!", true);
+            StringTokenizer st = new StringTokenizer(LinhaLida, " \":#()<=>+-?==@^&|~!", true);
 
             while (st.hasMoreTokens()) {
 
@@ -39,80 +38,98 @@ public class AnalisadorLex {
                         teste1 = st.nextToken();
                         if (teste1.equals("=")) {
                             junta = teste + teste1;
-                            s = identChar.geraToken(junta , counter);
+                            s = identChar.geraToken(junta, counter);
                             System.out.println(s);
                             entrou = true;
                         } else {
-                        	if (teste1.equals(" ") == false) {
-                            s = identChar.geraToken(teste , counter);
-                            System.out.println(s);
-                            teste = teste1;
-                        	}
+                            if (teste1.equals(" ") == false) {
+                                s = identChar.geraToken(teste, counter);
+                                System.out.println(s);
+                                teste = teste1;
+                            }
                         }
                     }
                     if (teste.equals("<")) {
                         teste1 = st.nextToken();
                         if (teste1.equals("=")) {
                             junta = teste + teste1;
-                            s = identChar.geraToken(junta , counter);
+                            s = identChar.geraToken(junta, counter);
                             System.out.println(s);
                             entrou = true;
                         } else {
-                        	if (teste1.equals(" ") == false) {
-                            s = identChar.geraToken(teste , counter);
-                            System.out.println(s);
-                            teste = teste1;               
-                        	}
+                            if (teste1.equals(" ") == false) {
+                                s = identChar.geraToken(teste, counter);
+                                System.out.println(s);
+                                teste = teste1;
+                            }
                         }
                     }
                     if (teste.equals("=")) {
                         teste1 = st.nextToken();
                         if (teste1.equals("=")) {
                             junta = teste + teste1;
-                            s = identChar.geraToken(junta , counter);
+                            s = identChar.geraToken(junta, counter);
                             System.out.println(s);
                             entrou = true;
                         } else {
-                        	if (teste1.equals(" ") == false) {
-                            s = identChar.geraToken(teste , counter);
-                            System.out.println(s);
-                            teste = teste1;
-                        	}
+                            if (teste1.equals(" ") == false) {
+                                s = identChar.geraToken(teste, counter);
+                                System.out.println(s);
+                                teste = teste1;
+                            }
                         }
                     }
                     if (teste.equals("!")) {
                         teste1 = st.nextToken();
                         if (teste1.equals("=")) {
                             junta = teste + teste1;
-                            s = identChar.geraToken(junta , counter);
+                            s = identChar.geraToken(junta, counter);
                             System.out.println(s);
                             entrou = true;
                         } else {
-                        	if (teste1.equals(" ") == false) {
-                            s = identChar.geraToken(teste , counter);
-                            System.out.println(s);
-                            teste = teste1;
-                        	}
+                            
+                            if (teste1.equals(" ") == false) {
+                                s = identChar.geraToken(teste, counter);
+                                System.out.println(s);
+                                teste = teste1;
+                            }else if (teste1.equals(" ") == true){
+                                s = identChar.geraToken(teste, counter);
+                                System.out.println(s);
+                                entrou = true;
+                            }
                         }
                     }
                     if (teste.equals(":")) {// :
                         teste1 = st.nextToken();// =
                         if (teste1.equals("=")) {// SE FOR =
                             junta = teste + teste1;// :=
-                            s = identChar.geraToken(junta , counter);
+                            s = identChar.geraToken(junta, counter);
                             System.out.println(s);
                             entrou = true;
                         } else {
-                        	if (teste1.equals(" ") == false) {
-                            s = identChar.geraToken(teste , counter);// :
-                            System.out.println(s);
-                            teste = teste1;// TESTE = '='
-                        	}
+                            if (teste1.equals(" ") == false) {
+                                s = identChar.geraToken(teste, counter);// :
+                                System.out.println(s);
+                                teste = teste1;// TESTE = '='
+                            }
                         }
+                    }
+                    if (teste.equals("\"") && st.hasMoreTokens()) {
+
+                        teste1 = st.nextToken();
+                        junta = teste + teste1;
+                        while (teste1.equals("\"") == false && st.hasMoreTokens()) {
+
+                            teste1 = st.nextToken();
+                            junta = junta + teste1;
+                        }
+                        s = identChar.geraToken(junta, counter);
+                        System.out.println(s);
+                        entrou = true;
                     }
 
                     if (entrou == false) {
-                        s = identChar.geraToken(teste , counter);
+                        s = identChar.geraToken(teste, counter);
                         System.out.println(s);
                     }
                 }
