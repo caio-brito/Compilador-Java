52b3dd7a7a105c1a1434fcd43286018b29ba3d8e
caiob <caiob@DESKTOP-RBLAGRM>
2022-10-20 15:35:38 -0300

    IMPLEMENTADAS todas as linhas da gramatica GYH. FALTANDO realizar a organização do codigo, testes de bugs e comentar

--- src/main/java/com/myname/analisadorlex/GyhSyntactic.java
+++ src/main/java/com/myname/analisadorlex/GyhSyntactic.java
@@ -56,14 +56,116 @@ public class GyhSyntactic {
         }
     }
 
+    public void ComandoCondicao(ArrayList<TipoToken> tokenList) {
+
+        ExpressaoRelacional(tokenList);
+
+        if (tokenList.get(index + 1).toString().equals("PCEntao")) {
+            index++;
+            ListaComandos(tokenList);
+            CondALinha(tokenList);
+        } else {
+            System.out.println("Erro, SE espera uma finalização ENTAO para as condições");
+        }
+    }
+
+    public void ComandoRepeticao(ArrayList<TipoToken> tokenList) {
+
+        ExpressaoRelacional(tokenList);
+        ListaComandos(tokenList);
+    }
+
+    public void ExpressaoRelacional(ArrayList<TipoToken> tokenList) {
+
+        TermoRelacional(tokenList);
+        if (tokenList.get(index + 1).toString().equals("OpBoolE") || tokenList.get(index + 1).toString().equals("OpBoolOU")) {
+            RelALinha(tokenList);
+        }
+    }
+
+    public void RelALinha(ArrayList<TipoToken> tokenList) {
+
+        OperadorBooleano(tokenList);
+        TermoRelacional(tokenList);
+        if (VerificaOP_REL(tokenList.get(index + 1).toString())) {
+            index++;
+            RelALinha(tokenList);
+        }
+    }
+
+    public void OperadorBooleano(ArrayList<TipoToken> tokenList) {
+
+        switch (tokenList.get(index + 1).toString()) {
+
+            case "OpBoolE":
+                index++;
+                break;
+
+            case "OpBoolOU":
+                index++;
+                break;
+
+            default:
+                System.out.println("Erro, é esperado um operador booleano (E ou OU)");
+                break;
+        }//End switch
+    }//End OperadorBooleano
+
+    public void TermoRelacional(ArrayList<TipoToken> tokenList) {
+
+        if (tokenList.get(index + 1).toString().equals("AbrePar")) {
+            index++;
+            ExpressaoRelacional(tokenList);
+
+            if (tokenList.get(index + 1).toString().equals("FechaPar")) {
+
+            } else {
+                System.out.println("Erro, esperado um fecha parenteses");
+            }
+        } else {
+
+            ExpressaoAritmetica(tokenList);
+            if (VerificaOP_REL(tokenList.get(index + 1).toString())) {
+                index++;
+                ExpressaoAritmetica(tokenList);
+            } else {
+                System.out.println("Erro, faltando um operador relacional na condição");
+            }
+        }
+    }
+
+    public boolean VerificaOP_REL(String siglaOP) {
+
+        switch (siglaOP) {
+
+            case "OpRelMenor":
+                return true;
+            case "OpRelMaior":
+                return true;
+            case "OpRelMenorIgual":
+                return true;
+            case "OpRelMaiorIgual":
+                return true;
+            case "OpRelIgual":
+                return true;
+            case "OpRelDif":
+                return true;
+        }
+        return false;
+    }
+
+    public void CondALinha(ArrayList<TipoToken> tokenList) {
+
+        if (tokenList.get(index + 1).toString().equals("PCSenao")) {
+            index++;
+            ListaComandos(tokenList);
+        }
+    }
+
     public void ExpressaoAritmetica(ArrayList<TipoToken> tokenList) {
 
         TermoAritmetico(tokenList);
         ExpressaoAritmeticaLinha(tokenList);
-
-//        if (tokenList.get(index + 1).toString().equals("+") || tokenList.get(index + 1).toString().equals("-")) {
-//
-//        }
     }//End ExpressaoAritmetica
 
     public void ExpressaoAritmeticaLinha(ArrayList<TipoToken> tokenList) {
@@ -130,13 +232,11 @@ public class GyhSyntactic {
                 break;
             case "(":
                 index++;
+                ExpressaoAritmetica(tokenList);
 
-                //chamar expressao aritmetica
-                break;
-
-            case ")":
-                index++;
-
+                if (!tokenList.get(index + 1).toString().equals("FechaPar")) {
+                    System.out.println("Erro, espera-se um fecha parenteses");
+                }
                 break;
 
             default:
@@ -147,26 +247,44 @@ public class GyhSyntactic {
 
     }
 
+    public void SubAlgoritmo(ArrayList<TipoToken> tokenList) {
+        ListaComandos(tokenList);
+
+        if (!tokenList.get(index + 1).toString().equals("FIM")) {
+            System.out.println("Erro, espera-se um FIM para completar o INI");
+        }
+        index++;
+    }
+
     public void ListaComandos(ArrayList<TipoToken> tokenList) {
-       
+
         index++;
         switch (tokenList.get(index).toString()) {
 
             case "PCImprimir":
                 ComandosSaida(tokenList);
-
                 break;
-            
+
             case "PCLer":
-
                 ComandoEntrada(tokenList);
-
                 break;
 
             case "Var":
                 ComandoAtribuicao(tokenList);
-
                 break;
+
+            case "PCSe":
+                ComandoCondicao(tokenList);
+                break;
+
+            case "PCEnqto":
+                ComandoRepeticao(tokenList);
+                break;
+
+            case "INI":
+                SubAlgoritmo(tokenList);
+                break;
+
             default:
                 break;
         }//End switch
